55c515f74302ae6d2342394f23c872c7f45ae5ab
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-10-12 15:09:30 +0300

    inital commit

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the nth number you want.");
+            Console.Write("n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(fibSeq(n));
+        }
+
+        public static int fibSeq(int n)
+        {
+            if (n > 2)
+                return fibSeq(n - 1) + fibSeq(n - 2);
+            else return 1;
+        }
+    }
+}
